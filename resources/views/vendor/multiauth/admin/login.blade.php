b9ca4b3b4fa522fec1e0a0215cfff39b10b7cadb
Jimmy Kiarie <jimmy@localhost.localdomain>
2020-06-28 15:54:38 +0300

    commit

--- resources/views/vendor/multiauth/admin/login.blade.php
+++ resources/views/vendor/multiauth/admin/login.blade.php
@@ -14,7 +14,7 @@
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="jimlaravel@gmail.com"
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="super@admin.com"
                                 {{-- <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" --}}
                                     required autofocus> @if ($errors->has('email'))
                                 <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
-                                    required value="password"> @if ($errors->has('password'))
+                                    required value="secret123"> @if ($errors->has('password'))
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span> @endif
